2a32d26659feab5b6d4c0d41e54e701e938c035f
Daniel-86 <dj@manoderecha.mx>
2012-10-13 16:44:40 -0500

    mejorado p1

--- p1.groovy
+++ p1.groovy
@@ -1,6 +1,8 @@
-def suma = 0
-for(i in 3..<1000){
-    if(i%3==0 || i%5==0)
-        suma += i
-}
-println "la suma de los multiplos es $suma"
+//def suma = 0
+//for(i in 3..<1000){
+//    if(i%3==0 || i%5==0)
+//        suma += i
+//}
+//println "la suma de los multiplos es $suma"
+
+(3..1000).collect{ (it%3 == 0 || it%5 == 0)? it: 0 }.sum()
